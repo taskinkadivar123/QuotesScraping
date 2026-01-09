4dbf5d1bb4cd82fd28dab98df28d95afdcd020b5
Taskin Kadivar <121847887+taskinkadivar123@users.noreply.github.com>
2026-01-09 13:02:49 +0530

    first commit

--- quotes/models.py
+++ quotes/models.py
@@ -1,8 +1,1 @@
-from django.db import models
 
-class Quote(models.Model):
-    text = models.TextField()
-    author = models.CharField(max_length=255)
-
-    def __str__(self):
-        return f"{self.text} - {self.author}"
